df4c21dd380b752b388e67e0d8f77252d33e3400
falentio <riv.kevinfalent.io@gmail.com>
2022-05-04 14:45:40 +0700

    anu

--- src/xorshift.ts
+++ src/xorshift.ts
@@ -11,7 +11,7 @@ export function createXorshift(seed: string) {
 	let state = seed.length ** 2;
 	for (const i of seed) {
 		state += xorshift(state);
-		state += xorshift(i.charCodeAt(0) ** 2) ** 2;
+		state += xorshift(i.charCodeAt(0) ** 2);
 	}
 	return () => {
 		state = xorshift(state);
