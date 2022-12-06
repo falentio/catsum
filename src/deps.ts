6d03747a0ffd7359806e4284e512485c14c5eaab
falentio <riv.kevinfalent.io@gmail.com>
2022-12-06 17:42:45 +0700

    -----------------------------r

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-import "https://deno.land/std@0.136.0/dotenv/load.ts";
+import "https://deno.land/std/dotenv/load.ts";
 export type { RouterContext } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 export {
 	Application,
@@ -6,6 +6,6 @@ export {
 	Router,
 	STATUS_TEXT,
 } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-export { default as ImgixClient } from "https://cdn.skypack.dev/@imgix/js-core@v3.6.0?dts";
-export { default as autoBind } from "https://cdn.skypack.dev/auto-bind@5.0.1?dts";
-export * as csv from "https://deno.land/std@0.136.0/encoding/csv.ts";
+export { default as ImgixClient } from "https://cdn.skypack.dev/@imgix/js-core?dts";
+export { default as autoBind } from "https://cdn.skypack.dev/auto-bind?dts";
+export * as csv from "https://deno.land/std/encoding/csv.ts";
