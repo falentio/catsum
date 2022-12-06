06a19d3da1ba4d76c154662ce4a0e330596a2aad
Kevin Falentio <riv.kevinfalent.io@gmail.com>
2022-12-06 17:24:51 +0700

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,11 +1,11 @@
 import "https://deno.land/std@0.136.0/dotenv/load.ts";
-export type { RouterContext } from "https://deno.land/x/oak@v11/mod.ts";
+export type { RouterContext } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 export {
 	Application,
 	Context,
 	Router,
 	STATUS_TEXT,
-} from "https://deno.land/x/oak@v11/mod.ts";
+} from "https://deno.land/x/oak@v11.1.0/mod.ts";
 export { default as ImgixClient } from "https://cdn.skypack.dev/@imgix/js-core@v3.6.0?dts";
 export { default as autoBind } from "https://cdn.skypack.dev/auto-bind@5.0.1?dts";
 export * as csv from "https://deno.land/std@0.136.0/encoding/csv.ts";
