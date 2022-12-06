06496f38082038c5ea1e19de15f2c3dec73b44b8
falentio <riv.kevinfalent.io@gmail.com>
2022-12-06 17:19:11 +0700

    -

--- src/deps.ts
+++ src/deps.ts
@@ -5,7 +5,7 @@ export {
 	Context,
 	Router,
 	STATUS_TEXT,
-} from "https://deno.land/x/oak@v10.5.1/mod.ts";
+} from "https://deno.land/x/oak@v11/mod.ts";
 export { default as ImgixClient } from "https://cdn.skypack.dev/@imgix/js-core@v3.6.0?dts";
 export * as csv from "https://deno.land/std@0.136.0/encoding/csv.ts";
 export { default as autoBind } from "https://cdn.skypack.dev/auto-bind@5.0.1?dts";
